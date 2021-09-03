a59502ebba6e0ddd69980b5a8a0de422b956fa19
Kelly <yijinwu5@gmail.com>
2021-09-03 18:43:40 +0800

    更新畫面

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var data = JSON.parse(localStorage.getItem("listData")) || [];
 document.querySelector(".btn-new").addEventListener("click", addTodos);
+var deleteallmBtn = document.querySelector("#deleteall");
+
 updateList(data); //頁面一載入更新畫面
 
 document
@@ -12,15 +14,16 @@ document
 
 function addTodos() {
   const inputValue = document.querySelector(".todo-input").value;
+  if (inputValue.trim().length === 0) {
+    alert("請輸入文字");
+    return;
+  } //確認是否為空值
   var listValue = {
     content: inputValue,
   };
   data.push(listValue);
   updateList(data); //把新資料更新跑迴圈
-  if (inputValue.trim().length === 0) {
-    alert("請輸入文字");
-    return;
-  } //確認是否為空值
+
   localStorage.setItem("listData", JSON.stringify(data));
   //把目前的資訊 存在listData key裡面
   document.querySelector(".todo-input").value = "";
@@ -48,6 +51,14 @@ function updateList(items) {
   `;
   }
   newTodo.innerHTML = str;
+
+  // // 大於三筆資料時 出現刪除全部按鈕
+
+  if (data.length < 4) {
+    deleteallmBtn.classList.add("hide");
+  } else {
+    deleteallmBtn.classList.remove("hide");
+  }
 }
 
 // 刪除代辦事項
@@ -55,20 +66,30 @@ document.querySelector(".todo-list").addEventListener("click", function (e) {
   if (e.target.nodeName != "BUTTON") {
     return;
   }
-  var msg = "確定刪除全部資料嗎?";
-  if (confirm(msg) == true) {
-    var index = e.target.dataset.index;
-    data.splice(index, 1); //刪除全域變數再把它重新設置道localstorage
-    localStorage.setItem("listData", JSON.stringify(data));
-  } else {
-    return false;
-  }
+
+  var index = e.target.dataset.index;
+  data.splice(index, 1); //刪除全域變數再把它重新設置道localstorage
+  localStorage.setItem("listData", JSON.stringify(data));
+
   updateList(data);
   // if (e.target.classList.contains("btn-delete")) {
   //   e.target.parentNode.remove(); //刪除畫面中
   // }
 });
+//刪除全部
+// var deleteallmBtn =document.querySelector("#deleteall");
+deleteallmBtn.addEventListener("click", function () {
+  let allmsg = "確定刪除全部資料嗎?";
+  if (confirm(allmsg) == true) {
+    data = [];
+  } else {
+    return false;
+  }
 
+  updateList(data);
+});
+
+updateList(data);
 // document
 //   .querySelector(".todo-list")
 //   .addEventListener("click", function (event) {
@@ -78,7 +99,10 @@ document.querySelector(".todo-list").addEventListener("click", function (e) {
 //     }
 //   });
 
+var isStart = false;
+
 document.getElementById("start").addEventListener("click", function () {
+  isStart = !isStart;
   if (isStart) {
     $("#start-icon").toggleClass("far fa-stop-circle", false); // 刪除 暫停classname
     $("#start-icon").toggleClass("fa-play-circle", true); // 新增 撥放classname
@@ -99,19 +123,16 @@ document.getElementById("start").addEventListener("click", function () {
       displayCurrentTimeLeftInSession();
     }, 1000);
   }
-
-  isStart = !isStart;
 });
 
-let workDuration = 1500;
+let workDuration = 1500; //60*25
 let currentTimeLeft = 1500;
 let breakDuration = 300;
 var intervalId;
-var isStart = false;
 
 const displayCurrentTimeLeftInSession = () => {
   const secondLeft = currentTimeLeft;
-  console.log(secondLeft);
+
   let result = "";
   const seconds = secondLeft % 60;
   const minutes = parseInt(secondLeft / 60) % 60;
